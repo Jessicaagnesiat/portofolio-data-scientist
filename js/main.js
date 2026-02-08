624997f04d1b107d2acab2c22cc8c8f5a2fb5855
Jessica Agnesia Tataung <jessicaagnesiat@gmail.com>
2026-02-08 11:22:11 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,291 @@
+/*===================== MENU SHOW Y HIDDEN =================*/
+const navMenu =document.getElementById('nav-menu'),
+      navToggle =document.getElementById('nav-toggle'),
+      navClose =document.getElementById('nav-close')
+
+/*==== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*==== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*===================== REMOVE MENU MOBILE =================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*===================== ACCORDION SKILLS =================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(let i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close')
+        this.parentNode.className = 'skills__content skills__open'
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+
+/*======*/
+document.addEventListener("DOMContentLoaded", () => {
+  const texts = ["Data Analyst", "Data Scientist"];
+  let textIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+
+  const typingElement = document.getElementById("typing");
+
+  function typeEffect() {
+    const currentText = texts[textIndex];
+
+    if (!isDeleting) {
+      typingElement.textContent = currentText.slice(0, ++charIndex);
+      if (charIndex === currentText.length) {
+        setTimeout(() => (isDeleting = true), 1200);
+      }
+    } else {
+      typingElement.textContent = currentText.slice(0, --charIndex);
+      if (charIndex === 0) {
+        isDeleting = false;
+        textIndex = (textIndex + 1) % texts.length;
+      }
+    }
+
+    setTimeout(typeEffect, isDeleting ? 60 : 100);
+  }
+
+  typeEffect();
+});
+
+
+/*===================== QUALIFICATION TABS =================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tabItem =>{
+            tabItem.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+/*===================== SERVICES MODAL =================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn,i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+/*===================== PORTFOLIO SWIPER =================*/
+let swiperPortfolio = new Swiper('.portfolio__container', {
+    loop: false,
+    spaceBetween: 40,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination", 
+        clickable: true,
+    },
+});
+
+/*===================== TESTIMONIAL =================*/
+/*let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+
+    pagination: {
+        el: ".swiper-pagination", 
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+});*/
+
+/*===================== SCROLL SECTIONS ACTIVE LINK =================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===================== CHANGE BACKGROUND HEADER =================*/
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*===================== SHOW SCROLL TOP =================*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*===================== DARK LIGHT THEME =================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+const contactForm = document.querySelector('.contact__form');
+
+contactForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const project = document.getElementById('project').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (!name || !email || !message) {
+        alert("Nama, Email, dan Message wajib diisi ya 🙂");
+        return;
+    }
+
+    const subject = encodeURIComponent(`Portfolio Contact: ${project || "-"}`);
+    const body = encodeURIComponent(
+        `Nama: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+    );
+
+    const gmailURL =
+        `https://mail.google.com/mail/?view=cm&fs=1&to=jessicaagnesiat@gmail.com&su=${subject}&body=${body}`;
+
+    window.open(gmailURL, "_blank");
+});
+
+const filterButtons = document.querySelectorAll('.portfolio__filter');
+const portfolioItems = document.querySelectorAll('.portfolio__content');
+
+filterButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // active state
+    filterButtons.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+
+    const filter = btn.dataset.filter;
+
+    portfolioItems.forEach(item => {
+      const categories = item.dataset.category;
+
+      if (filter === 'all' || categories.includes(filter)) {
+        item.classList.remove('hide');
+      } else {
+        item.classList.add('hide');
+      }
+    });
+
+    // 🔥 WAJIB: reset swiper setelah filter
+    swiperPortfolio.update();
+    swiperPortfolio.slideTo(0);
+  });
+});
+
+
+document.querySelectorAll('.view-more-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const details = btn.previousElementSibling;
+
+    details.classList.toggle('show');
+    btn.textContent = details.classList.contains('show')
+      ? 'View less'
+      : 'View more';
+  });
+});
